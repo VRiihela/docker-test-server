14f3632e84729369ae4e74e7b3cca30b53a20ea8
Ville <117075543+VRiihela@users.noreply.github.com>
2026-09-17 13:12:36 +0300

    added test route /about

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/about', (req, res) => {
+    res.send('test about');
+});
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
